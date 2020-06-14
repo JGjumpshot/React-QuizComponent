3ccd96681a6313eaaea0bacef52f9607ec060c3c
Jackson Gunther <jackson.gunther@yahoo.com>
2020-06-14 13:45:21 -0600

    Finished

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,12 +15,15 @@ class Quiz extends Component {
     })
   }
 
+  handleResetClick() {
+    this.setState({quiz_position: 1});
+  }
   render() {
     const isQuizEnd = ((this.state.quiz_position - 1) === (quizData.quiz_questions.length));
     console.log(isQuizEnd);
     return (
       <div>
-        {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+        {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
         
       </div>
     );
